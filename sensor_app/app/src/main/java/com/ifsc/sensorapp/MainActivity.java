bec5bc64a6377110090f62146a0571bb052ac8eb
Priscila <priscilagrazieleba@gmail.com>
2022-07-03 19:29:10 -0300

    compass

--- sensor_app/app/src/main/java/com/ifsc/sensorapp/MainActivity.java
+++ sensor_app/app/src/main/java/com/ifsc/sensorapp/MainActivity.java
@@ -7,12 +7,28 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener  {
+    TextView textView;
+    ImageView imageView;
+
     SensorManager sensorManager;
-    Sensor sensor;
-    TextView tvSensorLight;
+    Sensor accelerometer, magnetometer;
+
+    private float[] lastAccelerometer = new float[3];
+    private float[] lastMagnetometer = new float[3];
+    private float[] rotationMatrix = new float[9];
+    private float[] orientation = new float[3];
+
+    boolean isLastAccelerometerArrayCopied = false;
+    boolean isLastMagnetometerArrayCopied = false;
+
+    long lastUpdatedTime = 0;
+    float currentDegree = 0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,19 +36,69 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
 
         sensorManager = (SensorManager) getSystemService(getApplicationContext().SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-        tvSensorLight = findViewById(R.id.tvSensorLight);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+        textView = findViewById(R.id.textView);
+        imageView = findViewById(R.id.imageView);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        float currentValue = event.values[0];
-        tvSensorLight.setText(Float.toString(currentValue));
+        if (event.sensor == accelerometer) {
+            System.arraycopy(event.values, 0, lastAccelerometer, 0, event.values.length);
+            isLastAccelerometerArrayCopied = true;
+        } else if (event.sensor == magnetometer) {
+            System.arraycopy(event.values, 0, lastMagnetometer, 0, event.values.length);
+            isLastMagnetometerArrayCopied = true;
+        }
+
+        if (isLastMagnetometerArrayCopied && isLastAccelerometerArrayCopied && System.currentTimeMillis() - lastUpdatedTime > 250) {
+            SensorManager.getRotationMatrix(rotationMatrix, null, lastAccelerometer, lastMagnetometer);
+            SensorManager.getOrientation(rotationMatrix, orientation);
+
+            float azimuthInRadians = orientation[0];
+            float azimuthInDegrees = (float) Math.toDegrees(azimuthInRadians);
+
+            RotateAnimation rotateAnimation = new RotateAnimation(
+                    currentDegree,
+                    -azimuthInDegrees,
+                    Animation.RELATIVE_TO_SELF,
+                    0.5f,
+                    Animation.RELATIVE_TO_SELF,
+                    0.5f
+            );
+
+            rotateAnimation.setDuration(250);
+            rotateAnimation.setFillAfter(true);
+
+            imageView.startAnimation(rotateAnimation);
+            currentDegree = -azimuthInDegrees;
+            lastUpdatedTime = System.currentTimeMillis();
+
+            int x = (int)azimuthInDegrees;
+            textView.setText(x + "º");
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        sensorManager.unregisterListener(this, accelerometer);
+        sensorManager.unregisterListener(this, magnetometer);
     }
 }

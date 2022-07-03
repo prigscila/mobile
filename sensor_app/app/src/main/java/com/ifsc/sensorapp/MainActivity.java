3bb03760f7e7926a9b0549298eeafa2d8b52a7ea
Priscila <priscilagrazieleba@gmail.com>
2022-07-03 18:56:01 -0300

    adding light sensor

--- sensor_app/app/src/main/java/com/ifsc/sensorapp/MainActivity.java
+++ sensor_app/app/src/main/java/com/ifsc/sensorapp/MainActivity.java
@@ -2,13 +2,37 @@ package com.ifsc.sensorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener  {
+    SensorManager sensorManager;
+    Sensor sensor;
+    TextView tvSensorLight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(getApplicationContext().SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        tvSensorLight = findViewById(R.id.tvSensorLight);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float currentValue = event.values[0];
+        tvSensorLight.setText(Float.toString(currentValue));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
